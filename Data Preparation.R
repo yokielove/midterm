a433d91547732ad8b9a49c2304bce3e9926b9529
Hanxi Zhang <hanseezhang@gmail.com>
2015-10-18 17:10:48 -0400

    first commit

---
+++ Data Preparation.R
@@ -0,0 +1,52 @@
+#Data Preparation
+
+#choose a variable, and i choose obesity from RISKFACTORSANDACCESSTOCARE
+set.seed(50)
+Risk<-read.csv("RISKFACTORSANDACCESSTOCARE.csv",header=T,stringsAsFactors=FALSE)
+head(Risk)
+obesity<-Risk[,c("Obesity")]
+head(obesity)
+obesity<-na.omit(obesity)
+obesity<-scale(obesity)
+
+# Determine number of clusters,clusters for 2
+wss <- (nrow(obesity)-1)*sum(apply(obesity,5,var))
+for (i in 2:15) wss[i] <- sum(kmeans(obesity, 
+                                     centers=i)$withinss)
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares")
+
+# K-Means Cluster Analysis, clusters for 2
+fit <- kmeans(obesity, 2) # 2 cluster solution
+# get cluster means 
+aggregate(obesity,by=list(fit$cluster),FUN=mean)
+# append cluster assignment
+obesity <- data.frame(obesity, fit$cluster)
+
+
+dim(obesity)
+obesityk<-kmeans(x=obesity,centers=2)
+obesityk
+
+# Determine number of clusters,clusters for 5
+wss <- (nrow(obesity)-1)*sum(apply(obesity,5,var))
+for (i in 5:15) wss[i] <- sum(kmeans(obesity, 
+                                     centers=i)$withinss)
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares")
+
+# K-Means Cluster Analysis, clusters for 5
+fit <- kmeans(obesity, 5) # 2 cluster solution
+# get cluster means 
+aggregate(obesity,by=list(fit$cluster),FUN=mean)
+# append cluster assignment
+obesity <- data.frame(obesity, fit$cluster)
+
+
+dim(obesity)
+obesityk<-kmeans(x=obesity,centers=5)
+obesityk
+
+  
+  
+
