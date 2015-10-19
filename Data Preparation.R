ccc6477e1ff4cfeb8f2e7752398a69224443794d
Hanxi Zhang <hanseezhang@gmail.com>
2015-10-19 09:20:36 -0400

    commit

--- Data Preparation.R
+++ Data Preparation.R
@@ -3,7 +3,6 @@ library(plyr)
 #choose a variable, and i choose obesity from RISKFACTORSANDACCESSTOCARE
 set.seed(50)
 Risk<-read.csv("RISKFACTORSANDACCESSTOCARE.csv",header=T,stringsAsFactors=FALSE)
-attach(Risk)
 head(Risk)
 
 Risk[Risk=="-1111.1"] <- 0
@@ -11,17 +10,21 @@ Risk[Risk=="-1111.1"] <- 0
 head(Risk)
 summary(Risk)
 
+# Determine number of clusters, when it is 2
 obesity<-Risk[,c("Obesity")]
 head(obesity)
 obesity<-na.omit(obesity)
 obesity<-scale(obesity)
 
-# Determine number of clusters,clusters for 2
-wss <- (nrow(obesity)-1)*sum(apply(obesity,5,var))
-for (i in 2:15) wss[i] <- sum(kmeans(obesity, 
-                                     centers=i)$withinss)
-plot(1:15, wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares")
+wssplot <- function(obesity, nc=15, seed=1234){
+  wss <- (nrow(obesity)-1)*sum(apply(obesity,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(obesity, centers=i)$withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Within groups sum of squares")}
+
+wssplot(obesity)
 
 # K-Means Cluster Analysis, clusters for 2
 fit <- kmeans(obesity, 2) # 2 cluster solution
@@ -35,13 +38,6 @@ dim(obesity)
 obesityk<-kmeans(x=obesity,centers=2)
 obesityk
 
-# Determine number of clusters,clusters for 5
-wss <- (nrow(obesity)-1)*sum(apply(obesity,5,var))
-for (i in 5:15) wss[i] <- sum(kmeans(obesity, 
-                                     centers=i)$withinss)
-plot(1:15, wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares")
-
 # K-Means Cluster Analysis, clusters for 5
 fit <- kmeans(obesity, 5) # 2 cluster solution
 # get cluster means 
@@ -49,9 +45,7 @@ aggregate(obesity,by=list(fit$cluster),FUN=mean)
 # append cluster assignment
 obesity <- data.frame(obesity, fit$cluster)
 
-
 dim(obesity)
 obesityk<-kmeans(x=obesity,centers=5)
 obesityk
-
 
